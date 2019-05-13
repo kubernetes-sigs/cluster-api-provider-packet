9681768b12695d756c88c1c21cf93fadf8f7e5fb
Avi Deitcher <avi@deitcher.net>
2019-05-13 11:54:28 +0200

    Generate Cluster and Machine resources.

---
+++ pkg/apis/packet/v1alpha1/packetclusterproviderspec_types.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2019 Packet Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// PacketClusterProviderSpecSpec defines the desired state of PacketClusterProviderSpec
+type PacketClusterProviderSpecSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// PacketClusterProviderSpecStatus defines the observed state of PacketClusterProviderSpec
+type PacketClusterProviderSpecStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PacketClusterProviderSpec is the Schema for the packetclusterproviderspecs API
+// +k8s:openapi-gen=true
+type PacketClusterProviderSpec struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PacketClusterProviderSpecSpec   `json:"spec,omitempty"`
+	Status PacketClusterProviderSpecStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// PacketClusterProviderSpecList contains a list of PacketClusterProviderSpec
+type PacketClusterProviderSpecList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []PacketClusterProviderSpec `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&PacketClusterProviderSpec{}, &PacketClusterProviderSpecList{})
+}
