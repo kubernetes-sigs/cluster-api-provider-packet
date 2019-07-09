4c1fa5d93d7c2bcd152c9d73d2e4021746500fe2
Avi Deitcher <avi@deitcher.net>
2019-07-09 18:28:28 +0300

    Add ItemsInList; create ClusterProviderFromProviderConfig

--- pkg/cloud/packet/util/util.go
+++ pkg/cloud/packet/util/util.go
@@ -11,10 +11,19 @@ import (
 const (
 	machineUIDTag = "cluster-api-provider-packet:machine-uid"
 	clusterIDTag  = "cluster-api-provider-packet:cluster-id"
+	MasterTag     = "kubernetes.io/role:master"
+	WorkerTag     = "kubernetes.io/role:node"
 )
 
 func MachineProviderFromProviderConfig(providerConfig clusterv1.ProviderSpec) (*packetconfigv1.PacketMachineProviderConfig, error) {
 	var config packetconfigv1.PacketMachineProviderConfig
+	if err := yaml.Unmarshal(providerConfig.Value.Raw, &config); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+func ClusterProviderFromProviderConfig(providerConfig clusterv1.ProviderSpec) (*packetconfigv1.PacketClusterProviderSpec, error) {
+	var config packetconfigv1.PacketClusterProviderSpec
 	if err := yaml.Unmarshal(providerConfig.Value.Raw, &config); err != nil {
 		return nil, err
 	}
@@ -27,6 +36,7 @@ func GenerateClusterTag(ID string) string {
 	return fmt.Sprintf("%s:%s", clusterIDTag, ID)
 }
 
+// ItemInList checks if one item is in the list
 func ItemInList(list []string, item string) bool {
 	for _, elm := range list {
 		if elm == item {
@@ -34,4 +44,26 @@ func ItemInList(list []string, item string) bool {
 		}
 	}
 	return false
+}
+
+// ItemsInList checks if all items are in the list
+func ItemsInList(list []string, items []string) bool {
+	// convert the items against which we are mapping into a map
+	itemMap := map[string]bool{}
+	for _, elm := range items {
+		itemMap[elm] = false
+	}
+	// every one that is matched goes from false to true in the map
+	for _, elm := range list {
+		if _, ok := itemMap[elm]; ok {
+			itemMap[elm] = true
+		}
+	}
+	// go through the map; if any is false, return false, else all matched so return true
+	for _, v := range itemMap {
+		if !v {
+			return false
+		}
+	}
+	return true
 }
