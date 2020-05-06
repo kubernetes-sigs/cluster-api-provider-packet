c2376159777d5d6812f77964957661fc9c6f16e5
Avi Deitcher <avi@deitcher.net>
2020-05-06 12:27:34 +0300

    fix annotations so the CRDs are correct

--- api/v1alpha3/packetmachine_types.go
+++ api/v1alpha3/packetmachine_types.go
@@ -94,6 +94,13 @@ type PacketMachineStatus struct {
 
 // +kubebuilder:subresource:status
 // +kubebuilder:object:root=true
+// +kubebuilder:resource:path=packetmachines,scope=Namespaced,categories=cluster-api
+// +kubebuilder:storageversion
+// +kubebuilder:printcolumn:name="Cluster",type="string",JSONPath=".metadata.labels.cluster\\.x-k8s\\.io/cluster-name",description="Cluster to which this PacketMachine belongs"
+// +kubebuilder:printcolumn:name="State",type="string",JSONPath=".status.instanceState",description="Packet instance state"
+// +kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.ready",description="Machine ready status"
+// +kubebuilder:printcolumn:name="InstanceID",type="string",JSONPath=".spec.providerID",description="Packet instance ID"
+// +kubebuilder:printcolumn:name="Machine",type="string",JSONPath=".metadata.ownerReferences[?(@.kind==\"Machine\")].name",description="Machine object which owns with this PacketMachine"
 
 // PacketMachine is the Schema for the packetmachines API
 type PacketMachine struct {
