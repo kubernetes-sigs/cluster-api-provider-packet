8b6f4e735112c6898661e555f3687afdae270b9e
Gianluca Arbezzano <gianarb92@gmail.com>
2020-05-07 18:39:08 +0200

    handle more status coming from packet device api
    By default we mark a machine failed when it returns a status that we do
    not know. I think it is very reasonable. But we were not handled a few
    status: provisioning and queued.

--- api/v1alpha3/types.go
+++ api/v1alpha3/types.go
@@ -6,6 +6,11 @@ type PacketResourceStatus string
 var (
 	// PacketResourceStatus is the string representing a Packet resource just created and in a provisioning state.
 	PacketResourceStatusNew = PacketResourceStatus("new")
+	// PacketResourceStatusQueued is the string representing a Packet resource that is waiting in a queue to be created.
+	PacketResourceStatusQueued = PacketResourceStatus("queued")
+	// PacketResourceStatusQueued is the string representing a Packet resource
+	// that got picked from a worker that is not provisioning it.
+	PacketResourceStatusProvisioning = PacketResourceStatus("provisioning")
 	// PacketResourceStatusRunning is the string representing a Packet resource already provisioned and in a active state.
 	PacketResourceStatusRunning = PacketResourceStatus("active")
 	// PacketResourceStatusErrored is the string representing a Packet resource in a errored state.
