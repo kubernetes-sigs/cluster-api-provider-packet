ed7672eb9ed7dd83203b34805d0e7544d80f47b7
Gianluca Arbezzano <gianarb92@gmail.com>
2020-05-19 17:56:47 +0200

    buildx unauthorized
    During the last release gh action I got this error:
    
    ```
    server message: insufficient_scope: authorization failed
    ```
    
    Looking around I found this:
    
    https://github.com/docker/buildx/issues/202#issuecomment-597375245
    
    Also, -1 action to use

--- scripts/multi_arch_docker_release.sh
+++ scripts/multi_arch_docker_release.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 set -e
 
+# This has to be set otherwise the default driver will not work
 docker buildx create --use --name build --node build --driver-opt network=host
+
 docker buildx build --push --platform linux/${ARCH} \
     -t packethost/cluster-api-provider-packet:latest-${ARCH} \
     -t packethost/cluster-api-provider-packet:${TAG}-${ARCH} \
     -f ../../Dockerfile.goreleaser .
 
+# Update the manifest for the new release
+docker buildx imagetools create \
+  -t packethost/cluster-api-provider-packet:${TAG} \
+  packethost/cluster-api-provider-packet:${TAG}-${ARCH} \
+  packethost/cluster-api-provider-packet:${TAG}-${ARCH}
+
+docker buildx imagetools create \
+  -t packethost/cluster-api-provider-packet:latest \
+  packethost/cluster-api-provider-packet:latest-${ARCH} \
+  packethost/cluster-api-provider-packet:latest-${ARCH}
