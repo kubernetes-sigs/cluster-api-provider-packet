132eadef04c9052c308f3598cd9c88bd70626337
Chris Privitere <23177737+cprivitere@users.noreply.github.com>
2023-04-05 14:50:59 -0500

    Sync go_install script with upstream
    Signed-off-by: Chris Privitere <23177737+cprivitere@users.noreply.github.com>

--- scripts/go_install.sh
+++ scripts/go_install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2020 The Kubernetes Authors.
+# Copyright 2021 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ if [ -z "${GOBIN}" ]; then
   exit 1
 fi
 
-rm "${GOBIN}/${2}"* || true
+rm -f "${GOBIN}/${2}"* || true
 
 # install the golang module specified as the first argument
 go install "${1}@${3}"
